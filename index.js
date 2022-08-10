abd480cb868583dbd41b08627b3c6344b2378acb
Ben McShane <benjaminmartinmc@gmail.com>
2022-08-10 11:34:28 -0600

    commit tree d31fdca42e5719cbcb834097485f8e896070e665

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
-function addingEventListener() {
+const input = document.getElementById('button');
+function addingEventListener(button) {
+    input.addEventListener('click', clickAlert);
+    function clickAlert() {
+        alert('Click!');
+    }
 }
